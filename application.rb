439267cc0663cadadcf982f348f0d2e52b982829
AaronRodrigues <aaronrod@gmail.com>
2017-08-31 08:53:34 +0100

    Adds additional routes -get route for /reveiws/:id -put route -delete route

--- application.rb
+++ application.rb
@@ -31,3 +31,36 @@ post '/reviews' do
     json review.errors.full_messages
   end
 end
+
+get '/reviews/:id' do
+  content_type :json
+  
+  review = Review.get params[:id]
+  review.to_json
+end
+
+put '/reviews/:id' do
+  content_type :json
+
+  review = Review.get params[:id]
+  if review.update params[:review]
+    status 200
+    json "Review was updated"
+  else
+    status 500
+    json review.errors.full_messages
+  end
+end
+
+delete '/reviews/:id' do
+  content_type :json
+
+  review = Review.get params[:id]
+  if review.destroy
+    status 200
+    json "Review was removed"
+  else
+    status 500
+    json "There was a problem removing the review"
+  end
+end
